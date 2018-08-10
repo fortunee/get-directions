1895022a0db30bc9520a93a25ca941be9ed6e906
Fortune Ekeruo <legalmody@gmail.com>
2018-08-10 11:01:36 +0100

    add direction component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React, { Component } from 'react';
+import Direction from './components/Direction/Direction';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <section>
-          <label for='destination'>Destination: </label>
-          <br />
-          <br />
-          <input type='text' name='destination' />
-          <br />
-          <br />
-          <label for='destination'>Location: </label> <br />
-          <input type='text' name='location' />
-          <br />
-          <br />
-          <button>View Direction</button>
-        </section>
+        <Direction />
       </div>
     );
   }
---
+++ src/components/Direction/DirectionForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class DirectionForm extends Component {
+  render() {
+    return (
+        <section>
+          <label for='destination'>Destination: </label>
+          <br />
+          <input type='text' name='destination' />
+          <br />
+          <br />
+          <label for='destination'>Location: </label> <br />
+          <input type='text' name='location' />
+          <br />
+          <br />
+          <button>View Direction</button>
+      </section>
+    )
+  }
+}
