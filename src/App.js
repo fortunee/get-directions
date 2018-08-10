311a58e20c51e7e3e2d21e43eb276fb21451f09f
Fortune Ekeruo <legalmody@gmail.com>
2018-08-10 10:49:42 +0100

    add destination and location input fields

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section>
+          <label for='destination'>Destination: </label>
+          <br />
+          <br />
+          <input type='text' name='destination' />
+          <br />
+          <br />
+          <label for='destination'>Location: </label> <br />
+          <input type='text' name='location' />
+          <br />
+          <br />
+          <button>View Direction</button>
+        </section>
       </div>
     );
   }
